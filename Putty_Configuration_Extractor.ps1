231080e293c368ef8d345ba2dc5a6a1a5c371f4a
ScriptMan404 <45128030+ScriptMan404@users.noreply.github.com>
2021-09-09 17:41:12 +0200

    Delete Putty_Configuration_Extractor.ps1

--- Putty_Configuration_Extractor.ps1
+++
@@ -1,6 +0,0 @@
-# EXPORT PUTTY CONFIGURATIONS
-
-$DestinationFile = "C:\Putty_Configurations.reg"
-#$DestinationFile = "D:\Pierrick\INFORMATIQUE\ORDINATEUR\Config_Putty"
-
-reg export HKCU\Software\SimonTatham $DestinationFile
